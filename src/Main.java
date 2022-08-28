b0b83e809a2333442dadc97d1975b38da6064761
LeonLawrence <leonlawrence@outlook.com>
2022-08-28 19:24:23 +0100

    Project Setup
    project setup

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Leon Lawrence's Adventures");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+}
