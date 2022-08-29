92fe969c147d66211f7a2752f900c58084415452
LeonLawrence <leonlawrence@outlook.com>
2022-08-29 10:50:45 +0100

    Keyboard Input Implementation
    KeyHander class added for keyboard input. White character title no longer visible after keyboard input. It appears that the game crashes after an action is made.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,14 +15,21 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenWidth; // 768 pixels = 48 * 16
     final int screenHeight = tileSize * maxScreenHeight; // 576 pixels = 48 * 12
 
+    KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
 
-    public GamePanel() {
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
+    public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         // Improves game's rendering performance
         this.setDoubleBuffered(true); // If set to true, all the drawing from this component will be done in an offscreen painting buffer
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -42,17 +49,29 @@ public class GamePanel extends JPanel implements Runnable {
             repaint(); // calling the paintComponent method
         }
     }
-    public void update() {
 
+    public void update() {
+        // X value increases to the right
+        // Y value increases as they go down
+        if (keyHandler.upPressed == true) {
+            playerY = playerY - playerSpeed;
+        } else if (keyHandler.downPressed == true) {
+            playerY = playerY + playerSpeed;
+        } else if (keyHandler.leftPressed == true) {
+            playerX = playerX - playerSpeed;
+        } else if (keyHandler.rightPressed == true) {
+            playerX = playerX + playerSpeed;
+        }
     }
+
     public void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
 
-        Graphics2D graphics2D = (Graphics2D)graphics;
+        Graphics2D graphics2D = (Graphics2D) graphics;
 
         graphics2D.setColor(Color.white);
 
-        graphics2D.fillRect(100, 100, tileSize, tileSize);
+        graphics2D.fillRect(playerX, playerY, tileSize, tileSize);
 
         graphics2D.dispose();
     }
