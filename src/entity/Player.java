eccfacd8363043e7f347608f06d8cf26818db259
LeonLawrence <leonlawrence@outlook.com>
2022-08-30 09:09:22 +0100

    Sprite & Animation Update
    Sprite animation now appears to move when traveling in any direction. Bug that would cause the animation to continue running if a key press is not pressed is also fixed.

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -32,35 +32,45 @@ public class Player extends Entity {
 
         try {
 
-
-            up1 = ImageIO.read(getClass().getResourceAsStream("../res/player/up1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("../res/player/up2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("../res/player/down1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("../res/player/down2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("../res/player/left1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("../res/player/left2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("../res/player/right1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("../res/player/right2.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("../res/player/player_up1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("../res/player/player_up2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("../res/player/player_down1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("../res/player/player_down2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("../res/player/player_left1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("../res/player/player_left2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("../res/player/player_right1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("../res/player/player_right2.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void update() {
+        if (keyHandler.upPressed == true || keyHandler.downPressed == true || keyHandler.leftPressed == true || keyHandler.rightPressed == true) {
 
-        
-        if (keyHandler.upPressed == true) {
-            direction = "up";
-            y -= speed;
-        } else if (keyHandler.downPressed == true) {
-            direction = "down";
-            y += speed;
-        } else if (keyHandler.leftPressed == true) {
-            direction = "left";
-            x -= speed;
-        } else if (keyHandler.rightPressed == true) {
-            direction = "right";
-            x += speed;
+            if (keyHandler.upPressed == true) {
+                direction = "up";
+                y -= speed;
+            } else if (keyHandler.downPressed == true) {
+                direction = "down";
+                y += speed;
+            } else if (keyHandler.leftPressed == true) {
+                direction = "left";
+                x -= speed;
+            } else if (keyHandler.rightPressed == true) {
+                direction = "right";
+                x += speed;
+            }
+
+            spriteCounter++;
+            if (spriteCounter > 7) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
         }
     }
 
@@ -80,27 +90,36 @@ public class Player extends Entity {
                 }
                 break;
             case "down":
-                image = down1;
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
                 break;
             case "left":
-                image = left1;
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
                 break;
             case "right":
-                image = right1;
-                break;
-        }
-
-        spriteCounter++;
-        if (spriteCounter > 12) {
-            if (spriteNum == 1) {
-                spriteNum = 2;
-            } else if (spriteNum == 2) {
-                spriteNum = 1;
-            }
-            spriteCounter = 0;
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
         }
 
         graphics2D.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
 
     }
+
+//    public void draw(Graphics2D graphics2D) {
+//        graphics2D.setColor(Color.white);
+//        graphics2D.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
+//    }
 }
