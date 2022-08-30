4b2438be42452f89ccf0d86217cd752a06710e72
LeonLawrence <leonlawrence@outlook.com>
2022-08-30 14:59:57 +0100

    Created & Imported World Map Data

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,18 +4,24 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileManager {
 
     GamePanel gamePanel;
     Tile[] tile;
+    int mapTileNumber[][];
 
     public TileManager(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
 
         //update this to increase tile space.
         tile = new Tile[10];
+        mapTileNumber = new int[gamePanel.maxScreenWidth][gamePanel.maxScreenHeight];
 
         getTileImage();
     }
@@ -35,6 +41,37 @@ public class TileManager {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void loadMap() {
+        try {
+            InputStream inputStream = getClass().getResourceAsStream("../res/maps.txt");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            int row = 0;
+            int column = 0;
+
+            while (row < gamePanel.maxScreenHeight && row < gamePanel.maxScreenWidth) {
+                String line = bufferedReader.readLine();
+
+                while (row < gamePanel.maxScreenWidth) {
+                    String numbers[] = line.split(" ") ;
+
+                    int number = Integer.parseInt(numbers[column]);
+
+                    mapTileNumber[row][column] = number;
+                    row++;
+                }
+                if (row == gamePanel.maxScreenHeight) {
+                    row = 0;
+                    column++;
+                }
+            }
+            bufferedReader.close();
+
+        }catch (Exception e) {
+
+            }
     }
 
     public void draw(Graphics2D graphics2D) {
