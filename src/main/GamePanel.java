13f48566842865c6dfd6d598a5d14b132cfd5c50
LeonLawrence <leonlawrence@outlook.com>
2022-08-31 19:13:36 +0100

    Game Loop & Drawing Objects on Screen

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -33,5 +33,31 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        while(gameThread != null) {
+//            System.out.println("the game loop is running");
+
+
+            // 1 UPDATE: update information such as character position
+            update();
+
+            // 2 DRAW: draw the screen with the updated information
+            repaint();
+        }
+    }
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(100, 100, tileSize, tileSize);
+
+        g2.dispose();
     }
 }
