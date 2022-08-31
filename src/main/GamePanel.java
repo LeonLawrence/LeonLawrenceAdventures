f005cab1d5b79b7203c6b9fcb364073ee6d65e2c
LeonLawrence <leonlawrence@outlook.com>
2022-08-31 20:24:25 +0100

    Game Loop & FPS Display Update

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -41,21 +41,71 @@ public class GamePanel extends JPanel implements Runnable {
 
     }
 
+//    @Override
+//    public void run() {
+//
+//        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
+//        double nextDrawTime = System.nanoTime() + drawInterval;
+//
+//        while (gameThread != null) {
+//
+//            update();
+//
+//            repaint();
+//
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime = remainingTime/1000000;
+//
+//                if (remainingTime < 0) {
+//                    remainingTime = 0;
+//                }
+//
+//                Thread.sleep((long) remainingTime);
+//
+//                nextDrawTime += drawInterval;
+//
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
     @Override
     public void run() {
 
-        while(gameThread != null) {
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
 
-            // 1 UPDATE: update information such as character position
-            update();
+        while (gameThread != null) {
 
-            // 2 DRAW: draw the screen with the updated information
-            repaint();
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000d) ;
+            System.out.println("FPS: " + drawCount);
+            drawCount = 0;
+            timer = 0;
         }
     }
+
     public void update() {
         if (keyH.upPressed == true) {
-            playerX -= playerSpeed;
+            playerY -= playerSpeed;
         } else if (keyH.downPressed == true) {
             playerY += playerSpeed;
         } else if (keyH.leftPressed == true) {
@@ -69,7 +119,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         super.paintComponent(g);
 
-        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D g2 = (Graphics2D) g;
 
         g2.setColor(Color.white);
 
--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -15,16 +15,16 @@ public class KeyHandler implements KeyListener {
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
 
-        if (code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
+        if (code == KeyEvent.VK_W) {
             upPressed = true;
         }
-        if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
+        if (code == KeyEvent.VK_S) {
             downPressed = true;
         }
-        if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
+        if (code == KeyEvent.VK_A) {
             leftPressed = true;
         }
-        if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
+        if (code == KeyEvent.VK_D) {
             rightPressed = true;
         }
     }
@@ -33,16 +33,16 @@ public class KeyHandler implements KeyListener {
     public void keyReleased(KeyEvent e) {
         int code = e.getKeyCode();
 
-        if (code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
+        if (code == KeyEvent.VK_W) {
             upPressed = false;
         }
-        if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
+        if (code == KeyEvent.VK_S) {
             downPressed = false;
         }
-        if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
+        if (code == KeyEvent.VK_A) {
             leftPressed = false;
         }
-        if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
+        if (code == KeyEvent.VK_D) {
             rightPressed = false;
         }
     }
