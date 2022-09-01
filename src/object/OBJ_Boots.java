399fd98cbb19d90444a777f0140ef203053605ed
LeonLawrence <leonlawrence@outlook.com>
2022-09-01 22:07:11 +0100

    Backup Restore

---
+++ src/object/OBJ_Boots.java
@@ -0,0 +1,19 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJ_Boots extends SuperObject {
+
+    public OBJ_Boots() {
+
+        name = "Boots";
+
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/res/objects/boots.png"));
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
