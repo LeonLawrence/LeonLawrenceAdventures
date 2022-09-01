608e34a7ea681bee91d91c4e0fe4cd3798217f9a
LeonLawrence <leonlawrence@outlook.com>
2022-09-01 15:21:31 +0100

    Object Interaction Implementation

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -15,6 +15,8 @@ public class Player extends Entity {
 
     public final int screenX;
     public final int screenY;
+    int hasKey = 0;
+
 
     public Player(GamePanel gp, KeyHandler keyH) {
 
@@ -28,6 +30,8 @@ public class Player extends Entity {
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
         solidArea.width = 32;
         solidArea.height = 32;
 
@@ -79,6 +83,11 @@ public class Player extends Entity {
             collisionOn = false;
             gp.cChecker.checkTile(this);
 
+            // CHECK OBJECT COLLISION
+//            gp.cChecker.checkObject(this, true);
+            int objIndex = gp.cChecker.checkObject(this, true);
+            pickUpObject(objIndex);
+
             // IF COLLISION IS FALSE, PLAYER CAN MOVE
             if (collisionOn == false) {
 
@@ -107,6 +116,29 @@ public class Player extends Entity {
                     spriteNum = 1;
                 }
                 spriteCounter = 0;
+            }
+        }
+    }
+
+    public void pickUpObject(int i) {
+        if (i != 999) {
+//            gp.obj[i] = null;
+
+            String objectName = gp.obj[i].name;
+
+            switch (objectName) {
+                case "Key":
+                    hasKey++;
+                    gp.obj[i] = null;
+                    System.out.println("Key: " + hasKey);
+                break;
+                case "Door":
+                    if (hasKey > 0) {
+                        gp.obj[i] = null;
+                        hasKey--;
+                    }
+                    System.out.println("Key: " + hasKey);
+                break;
             }
         }
     }
--- src/object/OBJ_Door.java
+++ src/object/OBJ_Door.java
@@ -15,5 +15,6 @@ public class OBJ_Door extends SuperObject {
         } catch(IOException e) {
             e.printStackTrace();
         }
+        collision = true;
     }
 }
