423d4b28e289dfd3c2d8f9b4e201ef96fe72b779
LeonLawrence <leonlawrence@outlook.com>
2022-09-02 15:18:25 +0100

    Displaying Image Key Message

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -31,9 +31,11 @@ public class GamePanel extends JPanel implements Runnable {
     // SYSTEM
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
-    Sound sound = new Sound();
+    Sound music = new Sound();
+    Sound se = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
     public AssetSetter aSetter = new AssetSetter(this);
+    public UI ui = new UI(this);
     Thread gameThread;
 
     // ENTITY AND OBJECT
@@ -148,21 +150,24 @@ public class GamePanel extends JPanel implements Runnable {
         // PLAYER
         player.draw(g2);
 
+        // UI
+        ui.draw(g2);
+
         g2.dispose();
     }
 
     public void playMusic(int i) {
-        sound.setFile(i);
-        sound.play();
-        sound.loop();
+        music.setFile(i);
+        music.play();
+        music.loop();
     }
 
     public void stopMusic() {
-        sound.stop();
+        music.stop();
     }
 
     public void playSE(int i) {
-        sound.setFile(i);
-        sound.play();
+        se.setFile(i);
+        se.play();
     }
 }
---
+++ src/main/UI.java
@@ -0,0 +1,28 @@
+package main;
+
+import object.OBJ_Key;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class UI {
+
+    GamePanel gp;
+    Font arial_40;
+    BufferedImage keyImage;
+
+    public UI(GamePanel gp) {
+        this.gp = gp;
+
+        arial_40 = new Font("Arial", Font.PLAIN, 40);
+        OBJ_Key key = new OBJ_Key();
+        keyImage = key.image;
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.setFont(arial_40);
+        g2.setColor(Color.white);
+        g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null); // 24x24 pixels
+        g2.drawString("x " + gp.player.hasKey, 100, 80);
+    }
+}
