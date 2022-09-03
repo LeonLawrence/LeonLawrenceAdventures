632bbe2a26831f97ecf5424ae600d23c23cc0c8e
LeonLawrence <leonlawrence@outlook.com>
2022-09-03 23:51:20 +0100

    Scaling object Images

--- src/object/OBJ_Key.java
+++ src/object/OBJ_Key.java
@@ -1,18 +1,26 @@
 package object;
 
+import main.GamePanel;
+
 import javax.imageio.ImageIO;
 import java.io.IOException;
 
 public class OBJ_Key extends SuperObject {
 
-    public OBJ_Key() {
+
+    GamePanel gp;
+
+    public OBJ_Key(GamePanel gp) {
+
+        this.gp = gp;
 
         name = "Key";
 
         try {
             image = ImageIO.read(getClass().getResourceAsStream("/res/objects/key.png"));
+            uTool.scaleImage(image, gp.tileSize, gp.tileSize);
 
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
