6e1bc65041083cb84fa0a1215457f19731a380ae
LeonLawrence <leonlawrence@outlook.com>
2022-09-03 20:31:17 +0100

    Scaling player images

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -118,7 +118,7 @@ public class TileManager {
                     worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
                     worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
 
-                g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+                g2.drawImage(tile[tileNum].image, screenX, screenY, null);
             }
 
             worldCol++;
