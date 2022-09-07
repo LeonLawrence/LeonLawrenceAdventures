4aa2855f051777f1b89f33e10c07e1ac6930aa76
LeonLawrence <leonlawrence@outlook.com>
2022-09-07 09:30:20 +0100

    font imports and rendering improvements

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,17 +1,16 @@
 package main;
 
-import object.OBJ_Key;
-
 import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class UI {
 
     GamePanel gp;
     Graphics2D g2;
-    Font arial_40, arial_80B;
-    //    BufferedImage keyImage;
+//    Font arial_40, arial_80B;
+    Font maruMonica, purisaB;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
@@ -26,8 +25,20 @@ public class UI {
     public UI(GamePanel gp) {
         this.gp = gp;
 
-        arial_40 = new Font("Arial", Font.PLAIN, 40);
-        arial_80B = new Font("Arial", Font.BOLD, 80);
+        try {
+        InputStream is = getClass().getResourceAsStream("/res/font/MaruMonica.ttf");
+            maruMonica = Font.createFont(Font.TRUETYPE_FONT, is);
+            is = getClass().getResourceAsStream("/res/font/PurisaBold.ttf");
+            purisaB = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (FontFormatException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+//        arial_40 = new Font("Arial", Font.PLAIN, 40);
+//        arial_80B = new Font("Arial", Font.BOLD, 80);
+
 //        OBJ_Key key = new OBJ_Key(gp);
 //        keyImage = key.image;
     }
@@ -41,7 +52,12 @@ public class UI {
 
         this.g2 = g2;
 
-        g2.setFont(arial_40);
+//        g2.setFont(arial_40);
+//        g2.setFont(maruMonica);
+        g2.setFont(purisaB);
+
+        // BELOW LINE FOR RENDERING IMPROVEMENTS
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         g2.setColor(Color.white);
 
         // PLAY STATE
@@ -56,67 +72,6 @@ public class UI {
         if (gp.gameState == gp.dialogueState) {
             drawDialogueScreen();
         }
-
-
-//        if (gameFinished == true) {
-//            g2.setFont(arial_40);
-//            g2.setColor(Color.white);
-//
-//            String text;
-//            int textLength;
-//            int x;
-//            int y;
-//
-//            text = "You found the treasure!";
-//            // Returns the length of the text below
-//            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-//            x = gp.screenWidth/2 - textLength/2;
-//            y = gp.screenHeight/2 - gp.tileSize*3;
-//            g2.drawString(text, x, y);
-//
-//            text = "Your time is: " + dFormat.format(playTime) + "!";
-//            // Returns the length of the text below
-//            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-//            x = gp.screenWidth/2 - textLength/2;
-//            y = gp.screenHeight/2 - gp.tileSize*-1;
-//            g2.drawString(text, x, y);
-//
-//            g2.setFont(arial_80B);
-//            g2.setColor(Color.yellow);
-//            text = "Congratulations!";
-//            // Returns the length of the text below
-//            textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-//            x = gp.screenWidth/2 - textLength/2;
-//            y = gp.screenHeight/2 + gp.tileSize*-1;
-//            g2.drawString(text, x, y);
-//
-//            gp.gameThread = null;
-//
-//        } else {
-//
-//            g2.setFont(arial_40);
-//            g2.setColor(Color.white);
-//            g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null); // 24x24 pixels
-//            g2.drawString("x " + gp.player.hasKey, 100, 80);
-//
-//            // TIME
-//            playTime += (double) 1/60;
-//            g2.drawString("Time: " + dFormat.format(playTime), gp.tileSize*19, 80);
-//
-//
-//            // MESSAGE
-//            if (messageOn == true) {
-//                g2.setFont(g2.getFont().deriveFont(30F));
-//                g2.drawString(message, gp.tileSize / 2, gp.tileSize * 3);
-//
-//                messageCounter++;
-//
-//                if (messageCounter > 120) { // 120 = 60 FPS
-//                    messageCounter = 0;
-//                    messageOn = false;
-//                }
-//            }
-//        }
 
     }
 
